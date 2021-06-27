b82bba25ebd237261741acce04e1f37195077564
mkhsang <mkhsang@gmail.com>
2021-06-27 13:39:39 -0700

    update

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import json
-
+import logging
 
 app = Flask(__name__)
 
@@ -12,6 +12,8 @@ def status():
             mimetype='application/json'
     )
 
+    ##log line
+    app.logger.info('STATUS Request SUCCESSFUL!')
     return response
 
 
@@ -23,6 +25,8 @@ def metrics():
             status=200,
             mimetype='application/json'
     )
+    
+    app.logger.info('METRICS Request SUCCESSFUL!')
 
     return response
 
@@ -31,7 +35,14 @@ def metrics():
 
 @app.route("/")
 def hello():
+    app.logger.info('MAIN Request SUCCESSFUL!')
+
     return "Hello World!"
 
 if __name__ == "__main__":
+
+
+    ##stream logs to app.log file
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+    
     app.run(host='0.0.0.0')
